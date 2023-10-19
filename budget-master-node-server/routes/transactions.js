51a25d24f3354d25c4ebb7ff8392a19f018ee9f7
AmenToThatBro <joshcameronmorris@gmail.com>
2023-10-18 22:44:42 -0500

    Finished rough draft of route to request by category and sorted by Also finished rough draft of route to request a range based on a field

--- budget-master-node-server/routes/transactions.js
+++ budget-master-node-server/routes/transactions.js
@@ -3,106 +3,69 @@ const router = express.Router();
 
 // Import the Transaction model
 const Transaction = require('../models/Transaction');
-
-// Getting all
+// Either return all transactions or return a specific category of transactions custom sorted
 router.get('/', async (req, res) => {
-    console.log('/transactions/ ... hit')
-    try {
-        const transactions = await Transaction.find();
-        res.json(transactions);
-    } catch (err) {
-        res.status(500).json({ message: err.message })
-    }
-})
-router.get('/income', async (req, res) => {
-    console.log('/transactions/income ... hit')
-    try {
-        const transactions = await Transaction.find().where({ category: 'income' });
-        res.json(transactions);
-    } catch (err) {
-        res.status(500).json({ message: err.message })
-    }
-})
-router.get('/expense', async (req, res) => {
 
-    if(body) {
-        console.log('body exists');
-        const paramType = body.paramType;
-        const sortType = body.sortType;
+    if(!req.body) {
+        console.log('Body doesn\'t exist. returning all transactions')
         try {
-            const transactions = await Transaction.find().where({ category: 'expense'}).sort({ [paramType]: [sortType] })
-        }
-        catch (err){
-            res.status(500).json({message: err.message})
-        }
-    }
+            const transactions = await Transaction.find();
+            res.status(200).json(transactions);
+            return;
 
+        }   catch (err) {
+            res.status(500).json({ message: err.message })
+        }        
+    }
+    
+    const {category, sortBy, sortDirection} = req.body;
+
+    if(!category) {category = ''};
+    if(!sortBy) {sortBy = 'name'};
+    if(!sortDirection) {sortDirection = 'asc'};
 
     try {
-        const transactions = await Transaction.find().where({ category: 'expense' });
-        res.json(transactions);
+        const transactions = await Transaction.find().where({ category: [category] }).sort({ [sortBy]: [sortDirection] })
+        res.status(200).json(transactions)
     } catch (err) {
-        res.status(500).json({ message: err.message })
+         res.status(500).json({ message: err.message })
     }
 })
-// router.get('/expense, async (req, res) => {
-//     const acceptedTypes = [
-//         'asc',
-//         'desc',
-//     ]
 
-//     const paramType = req.params.paramType;
-//     const sortType = req.params.sortType;
-
-//     if(acceptedTypes.includes(paramType)){
-//         try{
-//             const transaction = await Transaction.find()
-//                 .sort({ [paramType]: [sortType] })
-
-//             res.json(transaction)
-//         } catch (err) {
-//             res.status(500).json({ message: err.message })
-//         }
-//     }
-//     else {
-//         res.json({ message: 'Lookup parameter is not an accepted value.'})
-//     }
-
-
-// })
-
-// Getting range based on paramType
-router.get('/params/:paramType/:param1/:param2', async (req, res) => {
+// Pulling a TYPE and grabbing a lower range and upper range.
+router.get('/range', async (req, res) => {
+    console.log('hit /range')
+    
     const acceptedTypes = [
         'transactionDate',
         'amount',
         'createdAt',
         'updatedAt'
     ]
-    const paramType = req.params.paramType
 
-    let typeAccepted = false
-    
-    acceptedTypes.map((type) => {
-        if (type == paramType) {
-            return typeAccepted = true           
+    const { type, lowerRange, upperRange, sortBy } = req.body;
+    console.log(acceptedTypes.includes(type));
+    if (acceptedTypes.includes(type)) {
+        if(!sortBy) sortBy = 'asc';
+        if(lowerRange && upperRange) {
+            try {
+                const transactions = await Transaction.find({ [type]: { $gte: lowerRange, $lte: upperRange } })
+                .sort({ [type]: [sortBy] })
+                res.status(200).json(transactions);
+
+            }   catch (err) {
+                    res.status(500).json({ message: err.message })
+            }
         }
-    })
+        else res.status(300).json({ message: 'Must include low and high range'})
 
-    if(typeAccepted){
-        try{
-            const transaction = await Transaction.find({ [paramType]: { $gte: req.params.param1, $lte: req.params.param2 }})
-                .sort({ [paramType]: 'asc' })
-
-            res.json(transaction)
-        } catch (err) {
-            res.status(500).json({ message: err.message })
-        }
     }
-    else {
-        res.json({ message: 'Lookup parameter is not an accepted value.'})
-    }
+    else res.status(300).json({ message: 'TYPE of request not accepted'})
 })
+
+// TODO //
+// Pulling an array of transactions for a specific time period based on a field parameter
+
 
 // router.get('/month', async (req, res) => {
 //     let month = req.body.month
