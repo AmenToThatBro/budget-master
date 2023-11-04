fe3df9c710e25ce24c7547967b1b7cfa816077cd
AmenToThatBro <joshcameronmorris@gmail.com>
2023-11-04 17:07:31 -0500

    Ditto

--- budget-master/src/Transactions.js
+++ budget-master/src/Transactions.js
@@ -6,31 +6,6 @@ export default function Transactions() {
 
     const [windowVisible, setWindowVisible] = useState(false);
     const [refresh, setRefresh] = useState(false);
-    // const { loading } = useFetch('http://localhost:8000');
-    // const [incomeTransactions, setIncomeTransactions] = useState([]);
-    // const [expenseTransactions, setExpenseTransactions] = useState([]);
-    // const [sortBy, setSortBy] = useState('transactionDate');
-    // const [sortDirection, setSortDirection] = useState('asc');
-
-    // useEffect(() => {
-    //     post(`/transactions/`, {
-    //         category: 'income',
-    //         sortBy: sortBy,
-    //         sortDirection: sortDirection,
-    //     })
-    //     .then(data => setIncomeTransactions(data))
-    //     .catch(err => console.log(err.message))
-
-    //     post(`/transactions/`, {
-    //         category: 'expense',
-    //         sortBy: sortBy,
-    //         sortDirection: sortDirection,
-    //     })
-    //     .then(data => setExpenseTransactions(data))
-    //     .catch(err => console.log(err.message))
-    //     // eslint-disable-next-line react-hooks/exhaustive-deps
-    // }, [sortBy, sortDirection, windowVisible])
-
 
     function handleAddTransactionButton (e) {
         setWindowVisible(!windowVisible);
@@ -43,10 +18,10 @@ export default function Transactions() {
                 <button className="add-trans-btn" id="add-trans-btn" onClick={handleAddTransactionButton} disabled={false}>Add Transaction</button>
 
                 {windowVisible && <AddTransactionWindow onWindowVisible={() => setWindowVisible(!windowVisible)} setRefresh={setRefresh} refresh={refresh}/>}
-                <div id='income'>
+                <div id='income' className='left-list transaction-bound'>
                     <TransactionList refresh={refresh}></TransactionList>
                 </div>    
-                <div id='expense'>          
+                <div id='expense' className='right-list transaction-bound'>          
                     <TransactionList refresh={refresh}></TransactionList>
                 </div>      
             </div>
